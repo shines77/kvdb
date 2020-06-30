3dabc62cfdea6bc2c4ef780f62685143916cb7ed
shines77 <gz_shines@msn.com>
2020-07-01 05:35:59 +0800

    * 修正 ROUND_TO_POWER2_TEST(name, N) 宏; * 添加 Linux 下的 print_passed(), print_failed();
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/kvdb-cli/kvdb-cli.cpp
+++ src/main/kvdb-cli/kvdb-cli.cpp
@@ -57,10 +57,12 @@ using namespace kvdb::client;
 
 #define ROUND_TO_POWER2_TEST(name, N) \
     printf("Test: [%s<%" PRIuSIZE ">] == %" PRIuSIZE ", %" PRIuSIZE ", ", #name, size_t(N), \
-           kvdb::compile_time::name##<N##>::value, \
-           kvdb::run_time::round_up_to_pow2(size_t(N##))); \
-    KV_TEST_EQUAL(kvdb::compile_time::name##<N##>::value, \
-                  kvdb::run_time::round_up_to_pow2(size_t(N##)))
+           kvdb::compile_time::name##<N>::value, \
+           kvdb::run_time::round_up_to_pow2(size_t(N))); \
+    KV_TEST_EQUAL(kvdb::compile_time::name##<N>::value, \
+                  kvdb::run_time::round_up_to_pow2(size_t(N)))
+
+#if defined(_WIN32) || defined(__cygwin__)
 
 void print_passed()
 {
@@ -75,6 +77,38 @@ void print_failed()
     printf("Failed");
     RecoverConsoleTextColor();
 }
+
+#elif defined(__linux__) || defined(__GNUC__) || defined(__clang__)
+
+//
+// See: https://blog.csdn.net/hejinjing_tom_com/article/details/12162491
+//
+// \033[30m Black \033[0m
+//
+
+void print_passed()
+{
+    printf("\033[32mPassed\033[0m");
+}
+
+void print_failed()
+{
+    printf("\033[31mFailed\033[0m");
+}
+
+#else
+
+void print_passed()
+{
+    printf("Passed");
+}
+
+void print_failed()
+{
+    printf("Failed");
+}
+
+#endif // _WIN32
 
 void print_passed_ln()
 {
