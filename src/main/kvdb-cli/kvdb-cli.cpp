400eb74edbfbd15da563e01b251f16e8a0c61d95
shines77 <gz_shines@msn.com>
2020-07-01 06:13:15 +0800

    清理一些没用的代码;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/kvdb-cli/kvdb-cli.cpp
+++ src/main/kvdb-cli/kvdb-cli.cpp
@@ -125,8 +125,66 @@ void print_failed_ln()
 
 void round_to_power2_test_1()
 {
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 0);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 1);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 2);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 3);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 4);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 5);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 6);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 7);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 8);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 9);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 10);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 11);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 12);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 13);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 14);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 15);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 16);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 17);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 18);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 19);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 20);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 21);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 22);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 23);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 24);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 25);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 26);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 27);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 28);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 29);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 30);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 31);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 32);
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 33);
+    // 0x7FFFFFFF
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 2147483647);
+    // 0x80000000
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 2147483648);
+    // 0x80000001
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 2147483649);
+    // 0x7FFFFFFFFFFFFFFEFULL
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 9223372036854775806);
+    // 0x7FFFFFFFFFFFFFFFULL
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 9223372036854775807);
+    // 0x8000000000000000ULL
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 9223372036854775808);
+    // 0x8000000000000001ULL
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 9223372036854775809);
+    // 0xFFFFFFFFFFFFFFFEULL
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 18446744073709551614);
+    // 0xFFFFFFFFFFFFFFFFULL
+    ROUND_TO_POWER2_TEST(round_up_to_pow2, 18446744073709551615);
+
+    printf("\n");
+}
+
+void round_to_power2_test_2()
+{
     ROUND_TO_POWER2_TEST(round_up_to_power2, 0);
-    //ROUND_TO_POWER2_TEST(round_up_to_power2, 1);
+    ROUND_TO_POWER2_TEST(round_up_to_power2, 1);
     ROUND_TO_POWER2_TEST(round_up_to_power2, 2);
     ROUND_TO_POWER2_TEST(round_up_to_power2, 3);
     ROUND_TO_POWER2_TEST(round_up_to_power2, 4);
@@ -181,71 +239,18 @@ void round_to_power2_test_1()
     printf("\n");
 }
 
-void round_to_power2_test_2()
-{
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 0);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 1);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 2);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 3);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 4);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 5);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 6);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 7);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 8);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 9);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 10);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 11);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 12);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 13);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 14);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 15);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 16);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 17);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 18);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 19);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 20);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 21);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 22);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 23);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 24);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 25);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 26);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 27);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 28);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 29);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 30);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 31);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 32);
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 33);
-    // 0x7FFFFFFF
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 2147483647);
-    // 0x80000000
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 2147483648);
-    // 0x80000001
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 2147483649);
-    // 0x7FFFFFFFFFFFFFFEFULL
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 9223372036854775806);
-    // 0x7FFFFFFFFFFFFFFFULL
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 9223372036854775807);
-    // 0x8000000000000000ULL
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 9223372036854775808);
-    // 0x8000000000000001ULL
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 9223372036854775809);
-    // 0xFFFFFFFFFFFFFFFEULL
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 18446744073709551614);
-    // 0xFFFFFFFFFFFFFFFFULL
-    ROUND_TO_POWER2_TEST(_round_up_to_power2, 18446744073709551615);
-
-    printf("\n");
-}
-
-int main(int argc, char * argv[])
+void integral_utils_test()
 {
     printf("integral_utils<size_t>::bits       = %" PRIuPTR "\n", detail::integral_utils<size_t>::bits);
     printf("integral_utils<size_t>::max_shift  = %" PRIuPTR "\n", detail::integral_utils<size_t>::max_shift);
     printf("integral_utils<size_t>::max_power2 = %" PRIuPTR "\n", detail::integral_utils<size_t>::max_power2);
     printf("integral_utils<size_t>::max_num    = %" PRIuPTR "\n", detail::integral_utils<size_t>::max_num);
     printf("\n");
+}
+
+int main(int argc, char * argv[])
+{
+    integral_utils_test();
 
     round_to_power2_test_1();
     round_to_power2_test_2();
--- src/main/kvdb/support/PowerOf2.h
+++ src/main/kvdb/support/PowerOf2.h
@@ -63,28 +63,6 @@ struct integral_utils {
     static const unsigned_type max_num = static_cast<unsigned_type>(-1);
 };
 
-template <>
-struct integral_utils<size_t> {
-    // max bits
-    static const size_t bits = sizeof(size_t) * 8;
-    static const size_t max_shift = bits - 1;
-    // 0x80000000UL;
-    static const size_t max_power2 = static_cast<size_t>(1UL) << max_shift;
-    // 0xFFFFFFFFUL;
-    static const size_t max_num = static_cast<size_t>(-1);
-};
-
-template <>
-struct integral_utils<ssize_t> {
-    // max bits
-    static const size_t bits = sizeof(ssize_t) * 8;
-    static const size_t max_shift = bits - 1;
-    // 0x80000000UL;
-    static const size_t max_power2 = static_cast<size_t>(1UL) << max_shift;
-    // 0xFFFFFFFFUL;
-    static const size_t max_num = static_cast<size_t>(-1);
-};
-
 } // namespace detail
 
 namespace run_time {
@@ -112,120 +90,27 @@ inline SizeType round_up_to_pow2(SizeType n) {
     static_assert(std::is_integral<SizeType>::value,
         "Error: round_up_to_pow2(SizeType n) -- SizeType must be a integral type.");
     typedef typename std::make_unsigned<SizeType>::type UnsignedSizeType;
-    UnsignedSizeType x = static_cast<UnsignedSizeType>(n);
-    if (is_pow2(x)) {
-        if (x == detail::integral_utils<UnsignedSizeType>::max_power2)
+    UnsignedSizeType u = static_cast<UnsignedSizeType>(n);
+    if (is_pow2(u)) {
+        if (u == detail::integral_utils<UnsignedSizeType>::max_power2)
             return static_cast<SizeType>(detail::integral_utils<UnsignedSizeType>::max_num);
-        else if (x == 0)
+        else if (u == 0)
             return static_cast<SizeType>(1);
         else
-            return static_cast<SizeType>(x * 2);
+            return static_cast<SizeType>(u * 2);
     }
     else {
-        x = static_cast<UnsignedSizeType>(x - 1);
-        x = x | (x >> 1);
-        x = x | (x >> 2);
-        x = x | (x >> 4);
-        x = x | (x >> 8);
-        x = x | (x >> 16);
-        return (x != detail::integral_utils<UnsignedSizeType>::max_num) ?
-            static_cast<SizeType>(x + 1) : static_cast<SizeType>(x);
-    }
-}
-
-template <>
-inline uint32_t round_up_to_pow2(uint32_t n) {
-    if (is_pow2(n)) {
-        if (n == detail::integral_utils<uint32_t>::max_power2)
-            return detail::integral_utils<uint32_t>::max_num;
-        else
-            return (n * 2);
-    }
-    else {
-        uint32_t x = n - 1;
-        x = x | (x >> 1);
-        x = x | (x >> 2);
-        x = x | (x >> 4);
-        x = x | (x >> 8);
-        x = x | (x >> 16);
-        return (x != detail::integral_utils<uint32_t>::max_num) ? (x + 1) : x;
-    }
-}
-
-template <>
-inline int32_t round_up_to_pow2(int32_t n) {
-    uint32_t x = static_cast<uint32_t>(n);
-    if (is_pow2(x)) {
-        if (x == detail::integral_utils<uint32_t>::max_power2)
-            return static_cast<int32_t>(detail::integral_utils<uint32_t>::max_num);
-        else
-            return static_cast<int32_t>(x * 2);
-    }
-    else {
-        if (n < 0) {
-            x = static_cast<uint64_t>(-n);
+        u = static_cast<UnsignedSizeType>(u - 1);
+        u = u | (u >> 1);
+        u = u | (u >> 2);
+        u = u | (u >> 4);
+        u = u | (u >> 8);
+        u = u | (u >> 16);
+        if (detail::integral_utils<UnsignedSizeType>::bits == 64) {
+            u = u | (u >> 32);
         }
-        x = x - 1;
-        x = x | (x >> 1);
-        x = x | (x >> 2);
-        x = x | (x >> 4);
-        x = x | (x >> 8);
-        x = x | (x >> 16);
-        if (n >= 0)
-            return static_cast<int32_t>(x + 1);
-        else
-            return -static_cast<int32_t>(x + 1);
-    }
-}
-
-template <>
-inline uint64_t round_up_to_pow2(uint64_t n) {
-    if (is_pow2(n)) {
-        if (n == detail::integral_utils<uint64_t>::max_power2)
-            return detail::integral_utils<uint64_t>::max_num;
-        else if (n == 0)
-            return uint64_t(1);
-        else
-            return (n * 2);
-    }
-    else {
-        uint64_t x = n - 1;
-        x = x | (x >> 1);
-        x = x | (x >> 2);
-        x = x | (x >> 4);
-        x = x | (x >> 8);
-        x = x | (x >> 16);
-        x = x | (x >> 32);
-        return (x != detail::integral_utils<uint64_t>::max_num) ? (x + 1) : x;
-    }
-}
-
-template <>
-inline int64_t round_up_to_pow2(int64_t n) {
-    uint64_t x = static_cast<uint64_t>(n);
-    if (is_pow2(x)) {
-        if (x == detail::integral_utils<uint64_t>::max_power2)
-            return static_cast<int64_t>(detail::integral_utils<uint64_t>::max_num);
-        else if (n == 0)
-            return int64_t(1);
-        else
-            return static_cast<int64_t>(x * 2);
-    }
-    else {
-        if (n < 0) {
-            x = static_cast<uint64_t>(-n);
-        }
-        x = x - 1;
-        x = x | (x >> 1);
-        x = x | (x >> 2);
-        x = x | (x >> 4);
-        x = x | (x >> 8);
-        x = x | (x >> 16);
-        x = x | (x >> 32);
-        if (n >= 0)
-            return static_cast<int64_t>(x + 1);
-        else
-            return -static_cast<int64_t>(x + 1);
+        return (u != detail::integral_utils<UnsignedSizeType>::max_num) ?
+            static_cast<SizeType>(u + 1) : static_cast<SizeType>(u);
     }
 }
 
@@ -250,89 +135,44 @@ struct is_power2<0> {
 
 //////////////////////////////////////////////////////////////////////////////////
 
-//
-// round_up_to_pow2<N>
-//
-
-template <size_t N>
-struct round_up_to_pow2_impl {
-    static const size_t value = (is_power2<N>::value ? N :
-        round_up_to_pow2_impl<N + 1>::value);
-};
-
-template <size_t N>
-struct round_up_to_pow2 {
-    static const size_t value = round_up_to_pow2_impl<N>::value;
-};
-
-//////////////////////////////////////////////////////////////////////////////////
-
-#if defined(_WIN64) || defined(_M_X64) || defined(_M_AMD64) || defined(_M_IA64) \
- || defined(__amd64__) || defined(__x86_64__)
-
-template <>
-struct round_up_to_pow2_impl<18446744073709551615ULL> {
-    static const size_t value = 18446744073709551615ULL;
-};
-
-template <>
-struct round_up_to_pow2<18446744073709551615ULL> {
-    static const size_t value = 18446744073709551615ULL;
-};
-
-#else
-
-template <>
-struct round_up_to_pow2_impl<4294967295UL> {
-    static const size_t value = 4294967295UL;
-};
-
-template <>
-struct round_up_to_pow2<4294967295UL> {
-    static const size_t value = 4294967295UL;
-};
-
-#endif // _WIN64 || _M_X64 || _M_AMD64
-
-//////////////////////////////////////////////////////////////////////////////////
-
 // Maxinum recursive depth is about 497 layer.
 
 template <size_t N, size_t Power2>
-struct round_up_to_power2_impl {
+struct round_up_to_pow2_impl {
     static const size_t max_power2 = detail::integral_utils<size_t>::max_power2;
     static const size_t max_num = detail::integral_utils<size_t>::max_num;
     static const size_t nextPower2 = (Power2 < max_power2) ? (Power2 << 1) : 0;
+
     static const bool too_large = (N >= max_power2);
     static const bool reach_limit = (Power2 == max_power2);
 
     static const size_t value = ((N >= max_power2) ? max_num :
            (((Power2 == max_power2) || (Power2 > N)) ? Power2 :
-            round_up_to_power2_impl<N, nextPower2>::value));
+            round_up_to_pow2_impl<N, nextPower2>::value));
 
     //static const size_t value = ((N >= max_power2) ? max_num :
-    //    ((Power2 > N) ? Power2 : round_up_to_power2_impl<N, nextPower2>::value));
+    //    ((Power2 > N) ? Power2 : round_up_to_pow2_impl<N, nextPower2>::value));
 };
 
 template <size_t N>
-struct round_up_to_power2_impl<N, 0> {
+struct round_up_to_pow2_impl<N, 0> {
     static const size_t value = detail::integral_utils<size_t>::max_num;
 };
 
 template <size_t N>
-struct round_up_to_power2 {
-    static const size_t value = round_up_to_power2_impl<N, 1>::value;
+struct round_up_to_pow2 {
+    static const size_t value = round_up_to_pow2_impl<N, 1>::value;
 };
 
 template <>
-struct round_up_to_power2<0> {
+struct round_up_to_pow2<0> {
     static const size_t value = 1;
 };
 
 //////////////////////////////////////////////////////////////////////////////////
 
 template <size_t N>
-struct _round_up_to_power2_impl {
+struct round_up_to_power2_impl {
     static const size_t max_num = detail::integral_utils<size_t>::max_num;
     static const size_t N1 = N - 1;
     static const size_t N2 = N1 | (N1 >> 1);
@@ -345,56 +185,41 @@ struct _round_up_to_power2_impl {
 };
 
 template <size_t N>
-struct _round_up_to_power2 {
+struct round_up_to_power2 {
     static const size_t max_power2 = detail::integral_utils<size_t>::max_power2;
     static const size_t max_num = detail::integral_utils<size_t>::max_num;
-    static const size_t value = (N < max_num) ? _round_up_to_power2_impl<N + 1>::value : max_num;
+    static const size_t value = (N < max_num) ? round_up_to_power2_impl<N + 1>::value : max_num;
 };
 
 template <>
-struct _round_up_to_power2<0> {
+struct round_up_to_power2<0> {
     static const size_t value = 1;
 };
 
 //////////////////////////////////////////////////////////////////////////////////
 
-//
-// round_down_to_pow2<N>
-//
-
-template <size_t N>
+template <size_t N, size_t Power2>
 struct round_down_to_pow2_impl {
-    static const size_t value = (is_power2<N>::value ? N :
-        round_down_to_pow2_impl<N - 1>::value);
+    static const size_t max_power2 = detail::integral_utils<size_t>::max_power2;
+    static const size_t max_num = detail::integral_utils<size_t>::max_num;
+    static const size_t nextPower2 = (Power2 < max_power2) ? (Power2 << 1) : 0;
+
+    static const bool too_large = (N >= max_power2);
+    static const bool reach_limit = (nextPower2 == max_power2);
+
+    static const size_t value = (too_large ? max_power2 :
+           ((N == Power2) ? N :
+            ((reach_limit || nextPower2 > N) ? Power2 :
+             round_down_to_pow2_impl<N, nextPower2>::value)));
 };
 
 template <size_t N>
 struct round_down_to_pow2 {
-    static const size_t value = round_down_to_pow2_impl<N>::value;
-};
-
-//////////////////////////////////////////////////////////////////////////////////
-
-template <size_t N, size_t Power2>
-struct round_down_to_power2_impl {
-    enum {
-        nextPower2 = Power2 * 2
-    };
-    static const bool too_large = (N >= detail::integral_utils<size_t>::max_power2);
-    static const bool reach_limit = (nextPower2 == detail::integral_utils<size_t>::max_power2);
-    static const size_t value = (too_large ? detail::integral_utils<size_t>::max_power2 :
-           ((N == Power2) ? N :
-            ((reach_limit || nextPower2 > N) ? Power2 :
-             round_down_to_power2_impl<N, nextPower2>::value)));
-};
-
-template <size_t N>
-struct round_down_to_power2 {
-    static const size_t value = (is_power2<N>::value ? N : round_down_to_power2_impl<N, 1>::value);
+    static const size_t value = (is_power2<N>::value ? N : round_down_to_pow2_impl<N, 1>::value);
 };
 
 template <>
-struct round_down_to_power2<0> {
+struct round_down_to_pow2<0> {
     enum { value = 0 };
 };
 
